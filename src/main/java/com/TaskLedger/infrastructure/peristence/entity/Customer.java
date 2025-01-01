804df450c81d10e44ddd48818b8f762cb0e26964
FVXOU <juanc.guerrerop@unac.edu.co>
2025-01-01 10:37:07 -0500

    add:  others entities

---
+++ src/main/java/com/TaskLedger/infrastructure/peristence/entity/Customer.java
@@ -0,0 +1,25 @@
+package com.TaskLedger.infrastructure.peristence.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", updatable = false, nullable = false, columnDefinition = "UUID")
+    private UUID id;
+
+    private String name;
+    private String identificationType;
+    private String identifier;
+}
---
+++ src/main/java/com/TaskLedger/infrastructure/peristence/entity/Supplier.java
@@ -0,0 +1,27 @@
+package com.TaskLedger.infrastructure.peristence.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Supplier {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", updatable = false, nullable = false, columnDefinition = "UUID")
+    private UUID id;
+
+    private String name;
+    private String description;
+    private String identificationType;
+    private String identifier;
+    private String category;
+}
